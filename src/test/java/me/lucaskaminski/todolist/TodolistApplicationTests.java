0ec5d979603ec47231667a26694c0b76fbfd90a3
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-10 21:33:14 -0300

    imported initializr structure

---
+++ src/test/java/me/lucaskaminski/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucaskaminski.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
