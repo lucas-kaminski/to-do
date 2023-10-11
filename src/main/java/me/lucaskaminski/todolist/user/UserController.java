38e9fed597e4dd4ae1df6cc57bdbe1b91df2e8b4
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-10 22:26:04 -0300

    created first user controller and model

---
+++ src/main/java/me/lucaskaminski/todolist/user/UserController.java
@@ -0,0 +1,19 @@
+package me.lucaskaminski.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @PostMapping("/")
+  public void create(@RequestBody UserModel user) {
+    System.out.println(user.name);
+    System.out.println(user.email);
+    System.out.println(user.password);
+  }
+
+}
---
+++ src/main/java/me/lucaskaminski/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package me.lucaskaminski.todolist.user;
+
+public class UserModel {
+  public String name;
+  public String email;
+  public String password;
+}
