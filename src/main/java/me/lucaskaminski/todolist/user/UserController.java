b9547ad592803a3e97782150f6973a23554bbac0
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-13 14:33:19 -0300

    repository implementation

--- src/main/java/me/lucaskaminski/todolist/user/UserController.java
+++ src/main/java/me/lucaskaminski/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package me.lucaskaminski.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel user) {
-
-    System.out.println(user.getUsername());
-    System.out.println(user.getName());
-    System.out.println(user.getPassword());
+  public UserModel create(@RequestBody UserModel user) {
+    var userCreated = this.userRepository.save(user);
+    return userCreated;
   }
-
 }
