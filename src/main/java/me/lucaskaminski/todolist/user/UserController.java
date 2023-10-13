05ffde96bdadac5c6be1f90f9275214934b264e5
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-13 14:44:18 -0300

    ResponseEntity handling

--- src/main/java/me/lucaskaminski/todolist/user/UserController.java
+++ src/main/java/me/lucaskaminski/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package me.lucaskaminski.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,14 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel user) {
+  public ResponseEntity<?> create(@RequestBody UserModel user) { 
+    var userExists = this.userRepository.findByUsername(user.getUsername());
+
+    if (userExists != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+    }
+
     var userCreated = this.userRepository.save(user);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
--- src/main/java/me/lucaskaminski/todolist/user/UserModel.java
+++ src/main/java/me/lucaskaminski/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,6 +20,7 @@ public class UserModel {
   private UUID id;
 
   // Don't define @Column if the column name is the same as the field name
+  @Column(unique = true)
   private String username;
   private String name;
   private String password;
