0378a58d4d3a5091661704646fa4eb24867f333e
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-14 20:21:48 -0300

    password hashing with BCrypt

--- src/main/java/me/lucaskaminski/todolist/user/UserController.java
+++ src/main/java/me/lucaskaminski/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -16,12 +18,16 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public ResponseEntity<?> create(@RequestBody UserModel user) { 
+  public ResponseEntity<?> create(@RequestBody UserModel user) {
     var userExists = this.userRepository.findByUsername(user.getUsername());
 
     if (userExists != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
     }
+
+    var passwordHash = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+
+    user.setPassword(passwordHash);
 
     var userCreated = this.userRepository.save(user);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
