53ad7c3e5601ea426b6f8c5c5cfc8343f71e5e48
Lucas Kaminski <lucas.kssilveira@gmail.com>
2023-10-14 21:37:30 -0300

    datetime validation at task creation

--- src/main/java/me/lucaskaminski/todolist/task/TaskController.java
+++ src/main/java/me/lucaskaminski/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package me.lucaskaminski.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +21,21 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel task, HttpServletRequest request) {
+  public ResponseEntity<?> create(@RequestBody TaskModel task, HttpServletRequest request) {
     var idUser = request.getAttribute("idUser");
     task.setIdUser(UUID.fromString(idUser.toString()));
 
+    var currentDateTime = LocalDateTime.now();
+    if (currentDateTime.isAfter(task.getStartAt())) {
+      return ResponseEntity.badRequest().body("Start date must be after current date");
+    }
+
+    if (task.getEndAt().isBefore(task.getStartAt())) {
+      return ResponseEntity.badRequest().body("End date must be after start date");
+    }
+
     var createdTask = this.taskRepository.save(task);
-    return createdTask;
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
   }
 
 }
